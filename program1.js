3a65e9e5852a7faf1105b88d5f8d592e80793c6d
webpandaaa <ankushrajsingh2004@gmail.com>
2024-10-08 13:54:23 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -22,6 +22,10 @@ var isValid = function(s) {
     }
 
     return stack.length === 0;
+
+    console.log(isValid("()"));
+    console.log(isValid("()[]{}"));  
+    console.log(isValid("(]")); 
     
 };
 
