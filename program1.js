6181fd0ecac82aca33ef6d4a05e1899ebef01c4e
webpandaaa <ankushrajsingh2004@gmail.com>
2024-10-08 13:51:14 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -9,6 +9,19 @@ var isValid = function(s) {
         '{': '}',
         '[': ']'
     };
+
+    for (let char of s) {
+        // If it's an open bracket, push it onto the stack
+        if (bracketMap[char]) {
+            stack.push(char);
+        } else {
+            // If it's a close bracket, check for matching open bracket
+            const lastOpenBracket = stack.pop();
+            if (bracketMap[lastOpenBracket] !== char) {
+                return false; // Mismatched brackets
+            }
+        }
+    }
     
 };
 
